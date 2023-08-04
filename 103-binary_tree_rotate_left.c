02e9094a3b53b26b29c9a3dc86db05f53b807ba7
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-08-04 08:58:00 +0300

    #103 & #104 fix, betty check

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,17 +7,17 @@
 */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *pivot;
+	binary_tree_t *pivot;
 
-    if (!tree && !tree->right)
-        return (NULL);
+	if (!tree && !tree->right)
+		return (NULL);
 
-    pivot = tree->right;
-    tree->right = pivot->left;
-    if (!pivot->left)
-        pivot->left->parent = tree;
-    pivot->left = tree;
-    pivot->parent = tree->parent;
-    tree->parent = pivot;
-    return (pivot);
+	pivot = tree->right;
+	tree->right = pivot->left;
+	if (!pivot->left)
+		pivot->left->parent = tree;
+	pivot->left = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -5,19 +5,19 @@
  * @tree: node of root.
  * Return: pointer to a new node
 */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *pivot;
+	binary_tree_t *pivot;
 
-    if (!tree && !tree->left)
-        return (NULL);
+	if (!tree && !tree->left)
+		return (NULL);
 
-    pivot = tree->left;
-    tree->left = pivot->right;
-    if (!pivot->right)
-        pivot->right->parent = tree;
-    pivot->right = tree;
-    pivot->parent = tree->parent;
-    tree->parent = pivot;
-    return (pivot);
+	pivot = tree->left;
+	tree->left = pivot->right;
+	if (!pivot->right)
+		pivot->right->parent = tree;
+	pivot->right = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }
